1a8b4736036eed3d49497d07d06f7fa7d73165db
franko <fstipanov@gmail.com>
2023-05-16 18:37:11 +0200

    display color shades

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,6 +1,12 @@
-const ColorList = () => {
+import { nanoid } from "nanoid"
+import SingleColor from "./SingleColor";
+
+const ColorList = ({ colors }) => {
     return (
-        <div>ColorList</div>
+        <section className="colors">
+            {colors.map((color, index) => <SingleColor key={nanoid()} color={color} index={index} />
+            )}
+        </section >
     )
 }
 export default ColorList
